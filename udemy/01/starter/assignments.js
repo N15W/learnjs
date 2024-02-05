5340e77029cab07c6d52f37645ddfa00ff9db938
Nate <96261317+N15W@users.noreply.github.com>
2024-02-04 16:39:06 -0800

    udemy cont'd

---
+++ udemy/01/starter/assignments.js
@@ -0,0 +1,31 @@
+const country = "United States";
+const continent = "North America";
+let population = 332;
+let isIsland = false;
+let language = "English";
+
+/*console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);*/
+
+let halvedPopulation = population / 2;
+console.log(halvedPopulation);
+console.log(population++);
+const populationFinland = 6;
+console.log(population > populationFinland);
+
+let averagePopulation = 33;
+console.log(population < averagePopulation);
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`
+console.log(description);
+
+
+
+
+
--- udemy/01/starter/script.js
+++ udemy/01/starter/script.js
@@ -1,2 +1,36 @@
+/*
 let js = "amazing";
-console.log("NATE");
+console.log("FIrsT LiNe");
+console.log(69);
+
+let firstName = "Nate";
+console.log(firstName);
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof javaScriptIsFun);
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+/*let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
