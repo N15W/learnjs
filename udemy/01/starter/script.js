5a4c5f6606428d4149caef6129474079b65f5a37
N15W <nateomew@gmail.com>
2024-02-07 17:19:49 -0800

    completed challenge 1 and 2

--- udemy/01/starter/script.js
+++ udemy/01/starter/script.js
@@ -1,67 +1,86 @@
-/*
-let js = "amazing";
-console.log("FIrsT LiNe");
-console.log(69);
-
-let firstName = "Nate";
-console.log(firstName);
-
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun);
-
-console.log(typeof javaScriptIsFun);
-
-javaScriptIsFun = "YES!";
-console.log(typeof javaScriptIsFun);
-
-let year;
-console.log(year);
-console.log(typeof year);
-
-year = 1991;
-console.log(typeof year);
-
-console.log(typeof null);
-*/
-
-/*let age = 30;
-age = 31;
-
-const birthYear = 1991;
-birthYear = 1990;*/
-
-// const now = 2037;
-// const ageJonas = now - 1991;
-// const ageSarah = now - 2018;
-// console.log(ageJonas, ageSarah);
-
-// const firstName = "Nate";
-// const job = "IT Consultant";
+// /*
+// let js = "amazing";
+// console.log("FIrsT LiNe");
+// console.log(69);
+//
+// let firstName = "Nate";
+// console.log(firstName);
+//
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+//
+// console.log(typeof javaScriptIsFun);
+//
+// javaScriptIsFun = "YES!";
+// console.log(typeof javaScriptIsFun);
+//
+// let year;
+// console.log(year);
+// console.log(typeof year);
+//
+// year = 1991;
+// console.log(typeof year);
+//
+// console.log(typeof null);
+// */
+//
+// /*let age = 30;
+// age = 31;
+//
+// const birthYear = 1991;
+// birthYear = 1990;*/
+//
+// // const now = 2037;
+// // const ageJonas = now - 1991;
+// // const ageSarah = now - 2018;
+// // console.log(ageJonas, ageSarah);
+//
+// // const firstName = "Nate";
+// // const job = "IT Consultant";
+// // const birthYear = 1989;
+// //
+// // const nate = `I'm ${firstName} and I work as a ${job}. For those who care, I was born in ${birthYear}.`;
+// // console.log(nate);
+//
+// const age = 5;
+//
+//
+// if (age >= 18) {
+//     console.log("You can start driving.");
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`You are too young. Wait another ${yearsLeft} years. :)`);
+// }
+//
 // const birthYear = 1989;
 //
-// const nate = `I'm ${firstName} and I work as a ${job}. For those who care, I was born in ${birthYear}.`;
-// console.log(nate);
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+//
+// console.log(century);
+//
 
-const age = 5;
+// conversion
+const inputYear = "1989";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Nate'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " years old.");
+console.log('23' - '10' - 3);
+console.log('23' * '2');
 
 
-if (age >= 18) {
-    console.log("You can start driving.");
-} else {
-    const yearsLeft = 18 - age;
-    console.log(`You are too young. Wait another ${yearsLeft} years. :)`);
-}
 
-const birthYear = 1989;
-
-let century;
-if (birthYear <= 2000) {
-    century = 20;
-} else {
-    century = 21;
-}
-
-console.log(century);
 
 
 
